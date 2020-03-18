e2fc414239f4492353c064ba5420eda485b50be8
Rashad Murad <rashad.murad2017@gmail.com>
2020-03-18 13:16:17 +0400

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let cv = document.getElementById ("cv");
+let salaryadd = document.getElementById ("salaryadd");
+let rate = document.getElementById ("rate");
+let tenor = document.getElementById ("tenor");
+let total = document.getElementById ("total");
+
+total.addEventListener('click', function(){
+    
+})
+
