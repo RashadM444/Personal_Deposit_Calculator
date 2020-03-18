aebe63671db227e06a27b5630cd9d4c0d281662d
Rashad Murad <rashad.murad2017@gmail.com>
2020-03-18 17:33:36 +0400

    commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-let total = document.getElementById ("total");
+let total = document.getElementById('total');
 
 total.addEventListener('click', function(){
     let cv = +(document.getElementById ("cv").value);
@@ -7,18 +7,17 @@ total.addEventListener('click', function(){
     let rate = +(document.getElementById ("rate").value);
     let tenor = +(document.getElementById ("tenor").value);
     let result = 0;
-    tenor = tenor - tenor%30;
-    result = cv*(Math.pow(1+rate/100/12,tenor));
-    result = result + salaryadd*findSalaryFormulaFactor(tenor,rate);
+    tenor = Math.floor(tenor/30);
+    result = cv*(Math.pow(1+(rate/100)/12,tenor)) + salaryadd*findSalaryFormulaFactor(tenor,rate);
     alert (result);
 })
 
 function findSalaryFormulaFactor(time, percent) {
     let factor = 0;
-    for (let t = time; t>0; t--) {
-        factor = factor + Math.pow(1+percent/12/100, time)
-        return factor
+    for (let t = time; t > 0; t--) {
+        factor += Math.pow(1+(percent/100)/12, t);
     }
-    
+    return factor;
 }
+
 
