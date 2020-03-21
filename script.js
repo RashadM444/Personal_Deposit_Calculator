9d7b48778c27fcd5bc2d1379d6ee66f2f51c09f3
Rashad Murad <rashad.murad2017@gmail.com>
2020-03-21 13:34:36 +0400

    commit

--- script.js
+++ script.js
@@ -7,24 +7,31 @@ total.addEventListener('click', function(){
     let rate = +(document.getElementById ("rate").value);
     let tenor = +(document.getElementById ("tenor").value);
     let result = 0;
-    if (cv > 0 && salaryadd >= 0 && rate > 0 && tenor > 0 && tenor % 1 == 0) {
+    if (cv > 0 && salaryadd >= 0 && rate > 0 && rate <= 100 && tenor > 0 && tenor % 1 == 0) {
         tenor = Math.floor(tenor/30);
         result = Math.floor(cv*(Math.pow(1+(rate/100)/12,tenor)) + salaryadd*findSalaryFormulaFactor(tenor,rate));
         alert (result);
+    } else if(cv <= 0 || isNaN(cv)) {
+        console.log (`Wrong entry has been made`);
+        console.log (NaN);
+        return document.getElementById ('error').innerHTML = 'You can only enter positive Initial Amount';
+    } else if (salaryadd < 0 || isNaN(salaryadd)) {
+        console.log (`Wrong entry has been made`);
+        console.log (NaN);
+        return document.getElementById ('error').innerHTML = 'Monthly added amount cannot be less than zero';
+    } else if (rate <= 0 || rate > 100 || isNaN(rate)) {
+        console.log (`Wrong entry has been made`);
+        console.log (NaN);
+        return document.getElementById ('error').innerHTML = 'Rate can only be a number between 0 and 100';
+    } else if (tenor <= 0 || tenor %1 !== 0 || isNaN (tenor)) {
+        console.log (`Wrong entry has been made`);
+        console.log (NaN);
+        return document.getElementById ('error').innerHTML = 'You can only enter positive time period with no decimals';
     } else {
         console.log (`Wrong entry has been made`);
         console.log (NaN);
-        return document.getElementById ('error').innerHTML = 'You can only enter positive numbers';
+        return document.getElementById ('error').innerHTML = 'Wrong entry format. Please try again.';
     }
-    
-    
-    
-    
-    
-    
-    
-    
-
 })
 
 function findSalaryFormulaFactor(time, percent) {
